e9f9a4c6208ae1913f8943461bc157b866a80e58
Jordan <jmillerchamp@gmail.com>
2025-12-22 14:04:57 -0600

    Remove

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray,num) {
+    const newArray = [];
+    for(let i= 0; i<oldArray.length;i++){
+        if(oldArray[i] != num){
+            newArray.push(oldArray[i]);
+        }
+    }
+    return newArray;
 };
+
+console.log(removeFromArray([1,2,3,4,5,6,5],5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
