c8f25e2ee1bf1e7003afc45e4649915178a80160
Jordan <jmillerchamp@gmail.com>
2025-12-22 19:29:58 -0600

    sumall

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+
+    let sum = 0;
+    let tempNum = 0;
+
+    if(num1 <= 0 || num2 <= 0){
+        return "ERROR";
+    }else if(num1 > num2 ){
+        num2 = tempNum;
+        num2 = num1;
+        num1 = tempNum;
+        
+    }
+
+
+
+    for(let i=num2;i>(num1-1);i--){
+        sum += i;
+
+    }
+    return sum;
 
 };
+
+console.log(sumAll(5,1));
 
 // Do not edit below this line
 module.exports = sumAll;
